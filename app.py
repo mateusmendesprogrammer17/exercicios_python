0a694c25b55ed8c18464eb2843d7aae06ac634f9
mateusmendesprogrammer17 <mateusmendesprogrammer17>
2024-10-13 11:26:46 -0300

    arquivos python adicionados

---
+++ app.py
@@ -0,0 +1,8 @@
+from utils import utilities
+
+def main():
+    print()
+    print(utilities.soma(10,20))
+    print(utilities.subtract(15,40))
+if __name__ =="__main__":
+    main()
---
+++ utils/utilities.py
@@ -0,0 +1,5 @@
+def soma(a,b):
+    return a+b
+subtract = lambda x,y:x-y
+
+multiplicate = lambda x,y:x*y
